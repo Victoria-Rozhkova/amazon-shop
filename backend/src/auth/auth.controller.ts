bd013edb26661b12ef0b17122dbe2bf92af89c1e
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2023-03-12 18:18:25 +0400

    added auth: register, login, access-token

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller, HttpCode } from '@nestjs/common'
+import { Body, Post, UsePipes } from '@nestjs/common/decorators'
+import { ValidationPipe } from '@nestjs/common/pipes'
+import { AuthDto } from './auth.dto'
+import { AuthService } from './auth.service'
+import { Auth } from './decorators/auth.decorator'
+import { RefreshTokenDto } from './dto/refresh-token.dto'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('register')
+	async register(@Body() dto: AuthDto) {
+		return this.authService.register(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() dto: AuthDto) {
+		return this.authService.login(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+  // @Auth()
+	@Post('login/access-token')
+	async getNewTokens(@Body() dto: RefreshTokenDto) {
+		return this.authService.getNewTokens(dto.refreshToken)
+	}
 }
