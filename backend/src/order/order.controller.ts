6af25bfcf67f9bb05e28bc6787dfcf6cce724980
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2023-03-14 18:55:45 +0400

    added order request: get all

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common'
+import { Auth } from './../auth/decorators/auth.decorator'
+import { Controller, Get } from '@nestjs/common'
 import { OrderService } from './order.service'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
 
-@Controller('order')
+@Controller('orders')
 export class OrderController {
 	constructor(private readonly orderService: OrderService) {}
+
+	@Get()
+	@Auth()
+	async getOrders(@CurrentUser('id') id: number) {
+		return this.orderService.getAll(id)
+	}
 }
--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -1,4 +1,16 @@
+import { PrismaService } from './../prisma.service'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+	constructor(private prisma: PrismaService) {}
+
+	async getAll(userId: number) {
+		return this.prisma.order.findMany({
+			where: { userId },
+			orderBy: {
+				createdAt: 'desc'
+			}
+		})
+	}
+}
