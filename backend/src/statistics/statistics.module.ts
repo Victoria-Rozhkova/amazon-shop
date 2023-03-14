362dfa1b7a57b16996a979b570a675ce30120d1a
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2023-03-14 18:53:38 +0400

    added statistics main request

--- backend/src/statistics/statistics.module.ts
+++ backend/src/statistics/statistics.module.ts
@@ -2,9 +2,10 @@ import { PrismaService } from 'src/prisma.service'
 import { Module } from '@nestjs/common'
 import { StatisticsService } from './statistics.service'
 import { StatisticsController } from './statistics.controller'
+import { UserService } from 'src/user/user.service'
 
 @Module({
 	controllers: [StatisticsController],
-	providers: [StatisticsService, PrismaService]
+	providers: [StatisticsService, PrismaService, UserService]
 })
 export class StatisticsModule {}
--- backend/src/statistics/statistics.service.ts
+++ backend/src/statistics/statistics.service.ts
@@ -1,4 +1,45 @@
+import { PrismaService } from './../prisma.service'
 import { Injectable } from '@nestjs/common'
+import { UserService } from 'src/user/user.service'
 
 @Injectable()
-export class StatisticsService {}
+export class StatisticsService {
+	constructor(
+		private prisma: PrismaService,
+		private userService: UserService
+	) {}
+
+	async getMain(userId: number) {
+		const user = await this.userService.byId(userId, {
+			orders: {
+				select: {
+					items: {
+						select: {
+							price: true
+						}
+					}
+				}
+			},
+			reviews: true
+		})
+
+		return [
+			{
+				name: 'Orders',
+				value: user.orders.length
+			},
+			{
+				name: 'Reviews',
+				value: user.reviews.length
+			},
+			{
+				name: 'Favorites',
+				value: user.favorites.length
+			},
+			{
+				name: 'Total amount',
+				value: 1000
+			}
+		]
+	}
+}
