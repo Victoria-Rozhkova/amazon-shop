d04957996e88d67d9bd7b28fe3e66e1e69dc5a6b
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2023-03-14 18:44:57 +0400

    added reviews requests: get all, create review

--- backend/src/review/review.service.ts
+++ backend/src/review/review.service.ts
@@ -1,4 +1,45 @@
+import { PrismaService } from './../prisma.service'
 import { Injectable } from '@nestjs/common'
+import { returnReviewObject } from './return-review-object'
+import { ReviewDto } from './review.dto'
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+	constructor(private prisma: PrismaService) {}
+
+	async getAll() {
+		return this.prisma.review.findMany({
+			orderBy: {
+				createdAt: 'desc'
+			},
+			select: returnReviewObject
+		})
+	}
+
+	async create(userId: number, dto: ReviewDto, productId: number) {
+		return this.prisma.review.create({
+			data: {
+				...dto,
+				product: {
+					connect: {
+						id: productId
+					}
+				},
+				user: {
+					connect: {
+						id: userId
+					}
+				}
+			}
+		})
+	}
+
+	async getAverageValueByProductId(productId: number) {
+		return this.prisma.review
+			.aggregate({
+				where: { productId },
+				_avg: { rating: true }
+			})
+			.then(data => data._avg)
+	}
+}
